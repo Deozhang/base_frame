511d827231d95b692dcabf283beee1e353bd9c17
eddie <eddiezings@gmail.com>
2017-06-15 17:06:52 +0800

    update

---
+++ js/default.js
@@ -0,0 +1,36 @@
+$(function () {
+    pageInit.setWidth();
+    pageInit.setSidebar();
+})
+$(window).resize(function () {
+    pageInit.setWidth();
+})
+
+/*
+ * init page when page load
+ */
+var pageInit = (function (page) {
+    page.setWidth = function () {
+        if ($(window).width() < 768) {
+            $(".sidebar").css({ left: -220 });
+            $(".main-wrap").css({ marginLeft: 0 });
+        } else {
+            $(".sidebar").css({ left: 0 });
+            $(".main-wrap").css({ marginLeft: 220 });
+        }
+    };
+    page.setSidebar = function(){
+        $('[data-target="sidebar"]').click(function(){
+            var asideLeft = $(".sidebar").offset().left;
+            if (asideLeft == 0) {
+                $(".sidebar").animate({ left: -220 });
+                $(".main-wrap").animate({ marginLeft: 0 });
+            }
+            else {
+                $(".sidebar").animate({ left: 0 });
+                $(".main-wrap").animate({ marginLeft: 220 });
+            }
+        });
+    }
+    return page;
+})(window.pageInit || {});
